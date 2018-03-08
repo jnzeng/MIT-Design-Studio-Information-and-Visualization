795fd5d177d9b715838a8a1790b93230c30f6036
jnzeng <jnzeng@mit.edu>
2018-03-08 12:27:20 -0500

    working progress new

--- Project_2/script.js
+++ Project_2/script.js
@@ -12,30 +12,3 @@ function draw(error,data){
 }
 
 
-// Define the gradient
-var gradient = svg.append("svg:defs")
-    .append("svg:linearGradient")
-    .attr("id", "gradient")
-    .attr("x1", "0%")
-    .attr("y1", "0%")
-    .attr("x2", "100%")
-    .attr("y2", "100%")
-    .attr("spreadMethod", "pad");
-
-// Define the gradient colors
-gradient.append("svg:stop")
-    .attr("offset", "0%")
-    .attr("stop-color", "#a00000")
-    .attr("stop-opacity", 1);
-
-gradient.append("svg:stop")
-    .attr("offset", "100%")
-    .attr("stop-color", "#aaaa00")
-    .attr("stop-opacity", 1);
-
-// Fill the circle with the gradient
-var circle = svg.append('circle')
-    .attr('cx', width / 2)
-    .attr('cy', height / 2)
-    .attr('r', height / 3)
-    .attr('fill', 'url(#gradient)');
