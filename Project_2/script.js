caec223b4b1df2155a8dc952f118bb11a9d51c7b
jnzeng <jnzeng@mit.edu>
2018-03-09 05:16:21 -0500

    working progress

--- Project_2/script.js
+++ Project_2/script.js
@@ -14,16 +14,59 @@ function draw(error, data){
     var fahrenheit = true;
     var nowTime = new Date(data.currently.time * 1000);
     var todayTemp = data.currently.temperature;
-   // mobile1.select('.todayTemp').text(Math.floor(todayTemp));
-    mobile1.select('.todayTemp').text(nowTime.getHours);
+   mobile1.select('.todayTemp').text(Math.floor(todayTemp));
+    mobile1.select('.iconName').text(data.currently.icon);
+    mobile1.select('.range').text('▲ ' + Math.floor(data.daily.data[0].temperatureHigh) + '  ▼ ' + Math.floor(data.daily.data[0].temperatureLow));
+    mobile1.select('.feelsLike').text(Math.floor(data.currently.apparentTemperature));
     
-    if (nowTime.getHours>=10 && nowTime.getHours <=24)
-        //mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+    var hours = nowTime.getHours;
+   // mobile1.select('.todayTemp').text(hours);
+//    if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//    if (nowTime.getHours>=2 && nowTime.getHours <=4)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #bcb8d1, #93a8d1, #93a8d1, #93a8d1)');
     
-    if (nowTime.getHours>=0 && nowTime.getHours <=10)
-        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #bcb8d1, #93a8d1, #93a8d1, #93a8d1)');
-    
+      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+       mobile1.select('.hour0').style('background', 'linear-gradient(to right, #bcb8d1, #93a8d1, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
+//      if (nowTime.getHours>=0 && nowTime.getHours <=2)
+//        mobile1.select('.hour0').style('background', 'linear-gradient(to right, #93a8d1, #93a8d1)');
+//    
     var todayIcon = data.currently.icon;
+    document.getElementById("weather").src = 'raining.gif';
+   // document.getElementById("weather").src = 'cloud.png';
+     //document.getElementById("weather").src = 'snow.png';
+    mobile1.select('.weeklyTemp').text(Math.floor(data.daily.data[1].temperatureLow));
+    mobile1.select('.weeklyHigh').text(Math.floor(data.daily.data[1].temperatureHigh));
+    
+     mobile1.select('.hour2').style('background', 'linear-gradient(to right, #bcb8d1, #93a8d1, #93a8d1, #93a8d1)');
+    
+//    mobile1.select('.weeklyTemp').text(Math.floor(data.daily.data[1].temperatureLow) + '        ' + Math.floor(data.daily.data[2].temperatureLow)+ '        ' + Math.floor(data.daily.data[3].temperatureLow)+ '        ' + Math.floor(data.daily.data[4].temperatureLow));
     
 
 }
